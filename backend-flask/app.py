24a5532f2ebdced71e27e24ad7ec5e53c631b830
Diego Perico <dialperico@gmail.com>
2023-07-14 16:11:07 +0000

    implement backend check for the token

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -15,6 +15,8 @@ from services.messages import *
 from services.create_message import *
 from services.show_activity import *
 
+from lib.cognito_jwt_token import CognitoJwtToken, extract_access_token, TokenVerifyError, FlaskAWSCognitoError
+
 # HoneyComb packages to initialize
 from opentelemetry import trace
 from opentelemetry.instrumentation.flask import FlaskInstrumentor
@@ -66,6 +68,12 @@ trace.set_tracer_provider(provider)
 tracer = trace.get_tracer(__name__)
 
 app = Flask(__name__)
+
+cognito_jwt_token = CognitoJwtToken(
+  user_pool_id=os.getenv("AWS_COGNITO_USER_POOL_ID"),
+  user_pool_client_id=os.getenv("AWS_COGNITO_USER_POOL_CLIENT_ID"),
+  region=os.getenv("AWS_DEFAULT_REGION")
+)
 
 # X-Ray Initialize
 XRayMiddleware(app, xray_recorder)
@@ -156,11 +164,21 @@ def data_create_message():
 @xray_recorder.capture('activities_home')
 def data_home():
   #data = HomeActivities.run(Logger=LOGGER)
-  app.logger.debug('AUTH HEADER ------')
-  app.logger.debug(
-    request.headers.get('Authorization')
-  )
-  data = HomeActivities.run()
+
+  access_token = extract_access_token(request.headers)
+  try:
+    claims = cognito_jwt_token.verify(access_token)
+    #Authenticated request
+    app.logger.debug('Authenticated')
+    app.logger.debug(claims)
+    app.logger.debug(claims['username'])
+    data = HomeActivities.run(cognito_user_id=claims['username'])
+  except TokenVerifyError as e:
+    #Unauthenticated request
+    app.logger.debug(e)
+    app.logger.debug('Unauthenticated')
+    data = HomeActivities.run()
+    
   LOGGER.info('Hello Cloudwatch! from  /api/activities/home')
   return data, 200
 
